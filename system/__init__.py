a183ac516bb915bb44894160dc38372a7f12e003
Bruno Nicholas <sbnibro256@gmail.com>
2019-09-20 11:59:56 +0300

    adding redirect message on load

--- system/__init__.py
+++ system/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from .routes.router import sys_app
 
 app = Flask(__name__)
+
+# capturing error of load
+@app.route('/', methods=['GET'])
+def welcome():
+	data = {
+		'success': 'welcome please follow the documentation spec',
+		'status': 201
+	}
+	return jsonify(data)
 
 # register a blueprint for the version
 # with the API standard
