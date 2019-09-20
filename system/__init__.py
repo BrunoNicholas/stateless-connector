a995ab6c1efdbed82e0b6511556db55147b691a1
Bruno Nicholas <sbnibro256@gmail.com>
2019-09-20 12:28:08 +0300

    adding linting

--- system/__init__.py
+++ system/__init__.py
@@ -4,6 +4,7 @@ from .routes.router import sys_app
 
 app = Flask(__name__)
 
+
 # capturing error of load
 @app.route('/', methods=['GET'])
 def welcome():
@@ -13,7 +14,9 @@ def welcome():
 	}
 	return jsonify(data)
 
+
 @app.route('/api', methods=['GET'])
+@app.route('/api/', methods=['GET'])
 def api_route():
 	data = {
 		'info': 'you are close, add a version to proceed',
