498df1aabf7095495a17fb0c31fc7a09e328da84
Bruno Nicholas <sbnibro256@gmail.com>
2019-09-23 15:52:36 +0300

    adding version

--- system/routes/router.py
+++ system/routes/router.py
@@ -9,7 +9,7 @@ sys_app = Blueprint('sys_app', __name__)
 @sys_app.route('/', methods=['GET'])
 def index():
 	data = {
-		'success': 'welcome to the test app',
+		'success': 'welcome to the test app v1',
 		'status': 200
 	}
 	return jsonify(data)
@@ -31,4 +31,33 @@ def add_nums():
 		}
 		return jsonify(data), 202
 
+
+@sys_app.route('/form/add',methods=['POST','GET'])
+def form_add():
+	if request.method == 'POST':
+		num1 = int(request.form['num1'])
+		num2 = int(request.form['num2'])
+		return '''{} + {} = {}'''.format(num1,num2,(num1+num2))
+
+	return '''
+	<!doctype html>
+	<html>
+		<head>
+			<title>My Good App!</title>
+		</head>
+		<body>
+			<form method="POST">
+				<input type="number" name="num1" placeholder="First number!"  style="min-width:150px;"><br>
+				<input type="number" name="num2" placeholder="Second number!" style="min-width:150px;"><br>
+				<input type="submit" value="ADD">
+			</form>
+		</body>
+	</html>
+	'''
+
+
+@sys_app.route('/data/inputs',methods=['POST','GET'])
+def input_data():
+	if request.method == 'POST':
+		data = request.get_json()
 
